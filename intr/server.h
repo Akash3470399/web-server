19211f56367de56251e452b3e6c86c615eae71db
Akash Bhagat <bhagatakash0123@gmail.com>
2025-03-25 12:22:12 +0530

    Added server_init()

--- intr/server.h
+++ intr/server.h
@@ -7,7 +7,7 @@ typedef struct Server Server;
 
 Connection *create_connection(char *addr, int port);
 
-Server *server_init(Connection *);
+Server *server_init(char *addr, int port);
 int server_run(Server *);
 int server_stop(Server *);
 
--- src/server.c
+++ src/server.c
@@ -1,29 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <poll.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 
 #include "server.h"
 #include "buffer.h"
 
-typedef struct{
-    int sock;
-}Connection;
+#define CONN_BACKLOG 20
+#define INIT_CTAB_LEN 1
+#define POLL_TIMEOUT 0
 
-
-typedef struct
+struct Connection
 {
-    Connection *selfconn;
-    Connection *connection_table;
-}Server;
+    struct pollfd pdf;
+    struct sockaddr_in saddr;
+};
 
+struct Server
+{
+    struct pollfd server_pfd;
+    struct pollfd ctab[INIT_CTAB_LEN];
+    Buffer **inbufs, **outbufs;
+    int ctlen;
+};
 
+Server *server_init(char *addr, int port)
+{
+    Server *s;
+    struct sockaddr_in saddr;
+    int sock;
 
-int moniter_active_connections(Connection *conn_tab, int nconns);
-int accept_new_connection(Connection *server_conn);
-int read_message_from_connection(Connection *rdfrom, Buffer *buf);
-int write_message_to_connection(Connection *writeto, Buffer *buf);
+    // TCP Socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("Socket cration failed\n");
+        return NULL;
+    }
 
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(port);
+    if (inet_aton(addr, &(saddr.sin_addr.s_addr)) == 0)
+    {
+        printf("Invalid address\n");
+        close(sock);
+        return NULL;
+    }
 
-Server *server_init(Connection *);
-int server_run(Server *);
+    if (bind(sock, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+    {
+        printf("Bind failed\n");
+        close(sock);
+        return NULL;
+    }
+
+    if (listen(sock, CONN_BACKLOG) < 0)
+    {
+        printf("Listen failed\n");
+        close(sock);
+        return NULL;
+    }
+
+    if (
+        ((s = (Server *)malloc(sizeof(Server))) == NULL) &&
+        ((s->inbufs = (Buffer **)calloc(INIT_CTAB_LEN, sizeof(Buffer *))) == NULL) &&
+        ((s->outbufs = (Buffer **)calloc(INIT_CTAB_LEN, sizeof(Buffer *))) == NULL)
+    )
+    {
+        close(sock);
+        return NULL;
+    }
+
+    s->ctab[s->ctlen].fd = sock;
+    s->ctab[s->ctlen].events = POLLIN;
+    s->ctab[s->ctlen++].revents = 0;
+    return s;
+}
+
+int server_run(Server *s);
+
 int server_stop(Server *);
-
